5c5fe16f186219e0658c4d245a58b525a479435e
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2019-02-17 12:42:02 +0100

    New theme, index page and social links

--- _pelican/content/pages/index.md
+++ _pelican/content/pages/index.md
@@ -1,3 +1,5 @@
 Title: Index
+URL:
+save_as: index.html
 
 Welcome to Naranjo's personal website.
--- _pelican/pelicanconf.py
+++ _pelican/pelicanconf.py
@@ -20,16 +20,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = ()
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('GitHub', 'https://github.com/fjfnaranjo'),
+          ('Stack Overflow', 'https://stackoverflow.com/users/302482/fjfnaranjo'),
+          ('LinkedIn', 'https://www.linkedin.com/in/fjfnaranjo/?locale=en_US'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = 'theme'
